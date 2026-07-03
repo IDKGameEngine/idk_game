2c5a7da6728d82d6d02e0cd08cfbf9e56c868a0b
mellic03 <mellic03@gmail.com>
2026-07-03 16:32:14 +1000

    mkpkg/unpkg

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,8 @@
 #include "idk/engine.hpp"
-
-#include "idk/core/cfgparser.hpp"
-#include "idk/core/file.hpp"
-#include "idk/core/threadpool.hpp"
-#include "idk/core/InplaceList.hpp"
-
-#include "idk/engine.hpp"
 #include "idk/engine/world.hpp"
 #include "idk/GameService.hpp"
 #include "idk/GfxService.hpp"
 #include "idk/IoService.hpp"
-
-alignas(max_align_t) static uint8_t gfxSrvBuf[sizeof(idk::GfxService)];
-alignas(max_align_t) static uint8_t ioSrvBuf[sizeof(idk::IoService)];
-alignas(max_align_t) static uint8_t gameSrvBuf[sizeof(idk::GameService)];
-
-
-// struct testArgType
-// {
-//     size_t offset;
-//     size_t length;
-//     uint8_t *data;
-// };
-
-// static void testFunc(void *ptr)
-// {
-//     auto &arg = *(testArgType*)ptr;
-//     VLOG_INFO("[testFunc] offset={}", arg.offset);
-//     for (size_t i=0; i<arg.length; i++)
-//     {
-//         arg.data[arg.offset + i] = 0xA0;
-//     }
-// }
 
 int main(int argc, char **argv)
 {
@@ -45,22 +16,11 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // static idk::core::ThreadPool threadpool;
-    // static uint8_t buf[2048];
-    // static testArgType argbuf[8];
-    // for (int i=0; i<8; i++)
-    // {
-    //     argbuf[i].offset = i*256;
-    //     argbuf[i].length = 256;
-    //     argbuf[i].data = buf;
-    //     threadpool.createTask(testFunc, &argbuf[i]);
-    // }
+    static idk::GfxService gfxSrv;
+    static idk::IoService ioSrv;
+    static idk::GameService gameSrv(&gfxSrv);
 
-    auto *gfxSrv = new (gfxSrvBuf) idk::GfxService();
-    auto *ioSrv = new (ioSrvBuf) idk::IoService();
-    auto *gameSrv = new (gameSrvBuf) idk::GameService(gfxSrv);
-
-    idk::Engine engine({ioSrv, gameSrv, gfxSrv});
+    idk::Engine engine({&ioSrv, &gameSrv, &gfxSrv});
 
     while (engine.running())
     {
---
+++ src/unpkg.cpp
@@ -0,0 +1,40 @@
+#include "idk/core/file.hpp"
+#include "idk/core/metric.hpp"
+#include "idk/core/IdkPkg.hpp"
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main(int argc, char **argv)
+{
+    std::string inpath = "";
+    bool inpathGood = false;
+
+    for (int i=1; i<argc; i++)
+    {
+        if ((i+1 < argc) && (std::string(argv[i]) == "-i"))
+        {
+            inpath = std::string(argv[i+1]);
+            inpathGood = true;
+        }
+    }
+
+    if (inpathGood == false)
+    {
+        fprintf(stderr, "Must provide input (-i INPUT_FILE)\n");
+        exit(1);
+    }
+
+    idk::FileReader fileRd(inpath.c_str());
+    idk::core::PkgReader pkgRd(fileRd.getData(), fileRd.getSize());
+
+    for (auto [name, data, size]: pkgRd)
+    {
+        fs::create_directories(fs::path(name).parent_path());
+        idk::FileWriter fileWt(name);
+        fileWt.write(data, size);
+        // printf("FILE %s, data, size: 0x%lx, %lu\n", name, (uint64_t)data, size);
+    }
+
+    return 0;
+}
