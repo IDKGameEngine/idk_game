c90cc54257a5a0f8b07e971b4ffb69ad68958501
mellic03 <mellic03@gmail.com>
2026-07-05 09:16:09 +1000

    woop

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include "idk/GameService.hpp"
 #include "idk/GfxService.hpp"
 
+// #include "idk/core/message/LocalPort.hpp"
+// static idk::message::LocalPortRegion<idk::gfx::MeshDescriptor> TEST_PORT_REGION;
+// using TestPortAddress = idk::message::LocalPortAddress<idk::gfx::MeshDescriptor, &TEST_PORT_REGION>;
+
 int main(int argc, char **argv)
 {
     (void)argc;
@@ -12,19 +16,15 @@ int main(int argc, char **argv)
 
     std::srand(clock());
 
-    // bool isClient = true;
-    bool isServer = (argc==2 && std::string(argv[1])=="--server");
-
-    static idk::engine::PlatformService platSrv(isServer == true);
+    static idk::engine::PlatformService platSrv(false);
     static idk::engine::EntityService entitySrv;
     static idk::engine::NetService netSrv;
     static idk::GfxService gfxSrv(&platSrv);
     static idk::GameService gameSrv;
     static idk::Engine engine({&platSrv, &entitySrv, &netSrv, &gameSrv, &gfxSrv});
 
-    if (isServer) { netSrv.startGameServer(); }
-    else          { netSrv.startGameClient(); }
-    
+    { netSrv.startGameClient(); }
+
     while (engine.running())
     {
         engine.update();
