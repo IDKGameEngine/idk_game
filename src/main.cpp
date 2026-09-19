de1eeab4f692dcd6ea0008658a7434e2e1b0e156
mellic03 <mellic03@gmail.com>
2026-09-19 23:40:51 +1000

    woop

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,20 @@
 #include "libidk/log.hpp"
 #include "libidk/platform-sdl3/PlatformContext.hpp"
 #include "libidk/platform/IAudioBackend.hpp"
-#include <steam/steam_api.h>
+#include "idk/engine/Engine.hpp"
 
-static void InitSteamLinuxRuntime()
-{
-    SteamErrMsg errMsg = { 0 };
-    if (SteamAPI_InitEx(&errMsg) != k_ESteamAPIInitResult_OK)
-    {
-        VLOG_FATAL("SteamAPI_InitEx() failure: {}", errMsg);
-    }
-}
 
 int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
 
-    if (std::getenv("SteamEnv"))
-    {
-        VLOG_INFO("Detected Steam runtime environment");
-        InitSteamLinuxRuntime();
-    }
-    else
-    {
-        VLOG_INFO("Detected native runtime environment");
-    }
-
     std::srand(clock());
-
 
     idk::PlatformConfig cfg{"GameWindow", 1280, 720};
     idk::PlatformContextSdl3 ctx(cfg);
+
+    idk::Engine engine(&ctx);
 
     auto *audio = ctx.getFeature<idk::IAudioBackend>();
     IDK_ASSERT(audio != nullptr, "RUH ROH");
@@ -41,10 +24,7 @@ int main(int argc, char **argv)
         audio->startSound(snd);
     }
 
-    while (ctx.running())
-    {
-        ctx.update();
-    }
+    engine.start();
 
     return 0;
 }
